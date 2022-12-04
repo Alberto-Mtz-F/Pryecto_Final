85d8fb4104a9c59eed986d7e157353c8bb3aa767
Alberto Mtz <jalberto.mf21@gmail.com>
2022-12-03 22:34:00 -0700

    Validaciones para los post

--- src/models/cliente.model.ts
+++ src/models/cliente.model.ts
@@ -1,7 +1,20 @@
-export interface IClient{
+import { IsEmail, IsString, IsNumberString, IsDateString } from "class-validator";
+
+export class IClient{
+    @IsString()
     nombre: string;
+
+    @IsEmail()
     correo: string;
+
+    @IsNumberString()
     telefono: string;
+
+    @IsString()
     domicilio: string;
+
+    @IsDateString()
     fecha_nacimiento: string;
 }
+
+
