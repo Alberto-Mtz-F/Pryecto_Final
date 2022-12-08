1b7940f8031f586b6378589c395b4ff6edc1fd58
Alberto Mtz <jalberto.mf21@gmail.com>
2022-12-07 22:32:07 -0700

    Correccines minimas para el 10

--- src/api/Cliente/cliente.controller.ts
+++ src/api/Cliente/cliente.controller.ts
@@ -1,5 +1,5 @@
 import { IClient } from './../../models/cliente.model';
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param } from '@nestjs/common';
 import { ClienteService } from './cliente.service';
 
 @Controller('cliente')
@@ -23,8 +23,14 @@ export class ClienteController {
         return this.clientService.getAll()
     }
 
-    @Get('/byID')
-    get(@Body() params: number){
+    @Get('/byID/:id')
+    get(@Param('id') param){
+        const cliente = this.clientService.getByID(param)
+        return cliente ?? "El cliente no existe"
+    }
+
+    @Get('/ReportebyID')
+    getReporte(@Body() params: number){
         this.clientService.getByID(params)
     }
 }
